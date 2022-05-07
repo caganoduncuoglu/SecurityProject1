f87ab7d071f8a90bf81545f41937263fd2024719
Melik Çağan Oduncuoğlu <49001811+caganoduncuoglu@users.noreply.github.com>
2022-05-07 22:30:11 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecurityProject1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
